a70ecdf7cf1d6c8b6436161a6f361a29741aeb86
David Manthey <david.manthey@kitware.com>
2015-03-30 21:04:53 -0400

    Add the ability to query history.
    Track up to a week of history, and add the ability to report it.  Also,
    added a --count option to get report load for a limited time.

--- loadavg.py
+++ loadavg.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import copy
+import json
 import multiprocessing
 import os
 import pprint
@@ -16,8 +17,15 @@ import win32event
 import win32service
 import win32serviceutil
 
-Verbose = 0
+
 DefaultPort = 24047
+
+        
+avgWeights = {
+    1:  {"bits": 11, "w": 1884},  # 1884 = (1<<11)/exp(5sec/60sec)
+    5:  {"bits": 11, "w": 2014},  # 2014 = (1<<11)/exp(5sec/300sec)
+    15: {"bits": 11, "w": 2037},  # 2037 = (1<<11)/exp(5sec/900sec)
+}
 
 
 class LoadAvgCollect(threading.Thread):
@@ -30,6 +38,8 @@ class LoadAvgCollect(threading.Thread):
         self.setDaemon(True)
         self.halt = False
         self.state = {}
+        self.history = {}
+        self.maxhistory = 86400 * 7 / 5  # 1 week
         self.verbose = verbose
         self.lock = threading.Lock()
         self.fields = {
@@ -37,11 +47,6 @@ class LoadAvgCollect(threading.Thread):
             "diskload": r"\LogicalDisk(_Total)\Current Disk Queue Length",
             "numproc": r"\System\Processes",
             "percent": r"\Processor(_Total)\% Processor Time",
-            }
-        self.weights = {
-            1:  {"bits": 11, "w": 1884},  # 1884 = (1<<11)/exp(5sec/60sec)
-            5:  {"bits": 11, "w": 2014},  # 2014 = (1<<11)/exp(5sec/300sec)
-            15: {"bits": 11, "w": 2037},  # 2037 = (1<<11)/exp(5sec/900sec)
             }
 
     def format(self, state=None, mode="load"):
@@ -53,28 +58,7 @@ class LoadAvgCollect(threading.Thread):
                      formats the combined load."""
         if not state:
             state = self.get()
-        if mode in ("disk", "diskload"):
-            return "%4.2f %4.2f %4.2f %d" % (
-                float(state["diskload_1"]) / (1 << self.weights[1]["bits"]),
-                float(state["diskload_5"]) / (1 << self.weights[5]["bits"]),
-                float(state["diskload_15"]) / (1 << self.weights[15]["bits"]),
-                state["diskload"])
-        if mode in ("proc", "procload"):
-            return "%4.2f %4.2f %4.2f %d/%d" % (
-                float(state["load_1"]) / (1 << self.weights[1]["bits"]),
-                float(state["load_5"]) / (1 << self.weights[5]["bits"]),
-                float(state["load_15"]) / (1 << self.weights[15]["bits"]),
-                state["load"],
-                state["numproc"])
-        return "%4.2f %4.2f %4.2f %d/%d" % (
-            (float(state["load_1"] + state["diskload_1"]) /
-             (1 << self.weights[1]["bits"])),
-            (float(state["load_5"] + state["diskload_5"]) /
-             (1 << self.weights[5]["bits"])),
-            (float(state["load_15"] + state["diskload_15"]) /
-             (1 << self.weights[15]["bits"])),
-            state["load"]+state["diskload"],
-            state["numproc"])
+        return formatState(state, mode)
 
     def get(self):
         """Get the current state of the data collection.
@@ -83,6 +67,14 @@ class LoadAvgCollect(threading.Thread):
         state = copy.copy(self.state)
         self.lock.release()
         return state
+
+    def getHistory(self):
+        """Get the current history of the data collection.
+        Exit:  history: history dictionary."""
+        self.lock.acquire()
+        history = copy.copy(self.history)
+        self.lock.release()
+        return history
 
     def run(self):
         """Collect the data."""
@@ -118,19 +110,24 @@ class LoadAvgCollect(threading.Thread):
             if loadFromPercent >= 1:
                 state["load"] += int(loadFromPercent)
                 loadFromPercent -= int(loadFromPercent)
+            state["residual"] = loadFromPercent
             for key in ("load", "diskload"):
                 value = state[key]
-                for avg in self.weights:
+                for avg in avgWeights:
                     akey = key+"_"+str(avg)
                     cur = state.get(akey, 0)
-                    bits = self.weights[avg]["bits"]
-                    w = self.weights[avg]["w"]
+                    bits = avgWeights[avg]["bits"]
+                    w = avgWeights[avg]["w"]
                     new = (cur*w+((1 << bits) - w) * (value << bits)) >> bits
                     state[akey] = new
             state["time"] = curtime
             self.lock.acquire()
             for key in state:
                 self.state[key] = state[key]
+                if not key in self.history:
+                    self.history[key] = []
+                self.history[key] = self.history[key][-self.maxhistory:]
+                self.history[key].append(state[key])
             self.lock.release()
             if self.verbose >= 2:
                 print "%5.3f %s" % (curtime, self.format(state, "load"))
@@ -185,6 +182,9 @@ class LoadAvgService(threading.Thread):
         elif cmd == "status":
             state = self.collector.get()
             result = pprint.pformat(state).strip()
+        elif cmd == "history":
+            history = self.collector.getHistory()
+            result = json.dumps(history)
         else:
             result = "failed - unknown command"
         rs, ws, es = select.select([], [sock], [], timeout)
@@ -247,12 +247,42 @@ class LoadAvgSvc(win32serviceutil.ServiceFramework):
         servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
                               servicemanager.PYS_SERVICE_STARTED,
                               (self._svc_name_, ''))
-        self.collector = LoadAvgCollect(verbose=Verbose)
+        self.collector = LoadAvgCollect()
         self.collector.start()
         self.service = LoadAvgService(0, DefaultPort, self.collector)
         self.service.start()
         while not self.service.halt:
             time.sleep(5)
+
+
+def formatState(state, mode="load"):
+    """Generate a loadavg string for the process load or the disk load.
+    Enter: state: a dictionary of the state to format.
+           mode: "disk" or "diskload" formats the disk load.  "proc" or
+                 "procload" formats the process load.  Anything else formats
+                 the combined load."""
+    if mode in ("disk", "diskload"):
+        return "%4.2f %4.2f %4.2f %d" % (
+            float(state["diskload_1"]) / (1 << avgWeights[1]["bits"]),
+            float(state["diskload_5"]) / (1 << avgWeights[5]["bits"]),
+            float(state["diskload_15"]) / (1 << avgWeights[15]["bits"]),
+            state["diskload"])
+    if mode in ("proc", "procload"):
+        return "%4.2f %4.2f %4.2f %d/%d" % (
+            float(state["load_1"]) / (1 << avgWeights[1]["bits"]),
+            float(state["load_5"]) / (1 << avgWeights[5]["bits"]),
+            float(state["load_15"]) / (1 << avgWeights[15]["bits"]),
+            state["load"],
+            state["numproc"])
+    return "%4.2f %4.2f %4.2f %d/%d" % (
+        (float(state["load_1"] + state["diskload_1"]) /
+         (1 << avgWeights[1]["bits"])),
+        (float(state["load_5"] + state["diskload_5"]) /
+         (1 << avgWeights[5]["bits"])),
+        (float(state["load_15"] + state["diskload_15"]) /
+         (1 << avgWeights[15]["bits"])),
+        state["load"]+state["diskload"],
+        state["numproc"])
 
 
 def query_service(cmd="load", port=DefaultPort):
@@ -274,32 +304,62 @@ def query_service(cmd="load", port=DefaultPort):
     if not len(ws):
         return "Failed - socket not receiving"
     sock.send(cmd)
-    rs, ws, es = select.select([sock], [], [], timeout)
-    if not len(rs):
-        return
-    data = sock.recv(4096)
-    return data.strip()
+    data = []
+    while True:
+        rs, ws, es = select.select([sock], [], [], timeout)
+        if not len(rs):
+            break
+        data.append(sock.recv(4096))
+        if not len(data[-1]):
+            break
+        timeout = 0
+    return ("".join(data)).strip()
 
 
-def query_service_loop(cmd="load", port=DefaultPort, interval=5,
-                       collector=None):
+def query_service_loop(cmd="load", opts={}, interval=5, collector=None,
+                       verbose=0):
     """Query the running or internal service repeatedly.
     Enter: cmd: the command to send to the service.
-           port: the port of the service to query.
+           opts: a dictionary of additional options, possible including port,
+                 count, and old.
            interval: frequency to query the service.
            collector: if not None, get the status from this collector rather
-                      than from a service port."""
+                      than from a service port.
+           verbose: verbosity level."""
     nexttime = time.time()
+    if opts.get("old", 0) > 0 and not collector:
+        try:
+            history = json.loads(query_service("history", opts.get(
+                "port", DefaultPort)))
+        except Exception:
+            history = {}
+        if "time" in history and len(history["time"]):
+            oldtime = nexttime - interval * opts["old"]
+            pos = -len(history["time"])
+            while oldtime < nexttime - interval * 0.5:
+                if oldtime >= history["time"][pos]:
+                    while (pos + 1 < 0 and
+                            oldtime >= history["time"][pos + 1]):
+                        pos += 1
+                    state = {}
+                    for key in history:
+                        if len(history[key]) >= -pos:
+                            state[key] = history[key][pos]
+                    print "%s %s" % (time.strftime(
+                        "%H:%M:%S", time.localtime(oldtime)),
+                        formatState(state))
+                oldtime += interval
     first = True
+    count = opts.get("count", 0)
     while True:
         try:
             if not first or cmd != "run":
                 curtime = time.time()
                 if not collector:
-                    val = query_service(cmd, port)
+                    val = query_service(cmd, opts.get("port", DefaultPort))
                 else:
                     val = collector.format(mode=cmd)
-                if Verbose:
+                if verbose:
                     print "%s %s" % (time.strftime(
                         "%H:%M:%S", time.localtime(curtime)), val)
                 else:
@@ -307,6 +367,10 @@ def query_service_loop(cmd="load", port=DefaultPort, interval=5,
                     sys.stdout.flush()
             first = False
             nexttime += interval
+            if count:
+                count -= 1
+                if count <= 0:
+                    break
             delay = nexttime-time.time()
             if delay > 0:
                 time.sleep(delay)
@@ -317,24 +381,30 @@ def query_service_loop(cmd="load", port=DefaultPort, interval=5,
 if __name__ == "__main__":
     help = False
     mode = "load"
-    port = DefaultPort
     frequency = None
     collector = None
     windowsService = False
+    opts = {"port": DefaultPort}
+    verbose = 1
     for arg in sys.argv[1:]:
         if ((arg[:2] == "--" or arg[:1] == "/") and arg.lstrip("-/") in
-                ("disk", "load", "proc", "service", "end", "run", "status")):
+                ("disk", "load", "proc", "service", "end", "run", "status",
+                 "history")):
             mode = arg.lstrip("-/")
+        elif ((arg[:2] == "--" or arg[:1] == "/") and "=" in arg and
+                arg.lstrip("-/").split("=", 1)[0] in ("count", "old", "port")):
+            parts = arg.lstrip("-/").split("=", 1)
+            opts[parts[0]] = int(parts[1])
         elif arg in ("--browse", "/browse"):
             def print_counter(counter):
                 print counter
             win32pdh.BrowseCounters(
                 None, 0, print_counter, win32pdh.PERF_DETAIL_WIZARD,
                 "Counter List")
-        elif arg.startswith == "--port=" or arg.startswith("/port="):
-            port = int(arg.split("=", 1)[1])
-        elif arg in ("-v", "/v"):
-            Verbose += 1
+        elif arg in ("-q", "/q", "--quiet", "/quiet"):
+            verbose -= 1
+        elif arg in ("-v", "/v", "--verbose", "/verbose"):
+            verbose += 1
         elif arg in ['install', 'remove', 'start', 'stop']:
             windowsService = True
         elif not frequency and arg.isdigit():
@@ -344,25 +414,33 @@ if __name__ == "__main__":
     if help:
         print """Run or query a loadavg service on windows.
 
-Syntax: loadavg.py --load|--disk|--proc|--service|--end|--run|--status
-                   --port=(port) --browse -v (frequency)
-                   install|remove|start|stop
+Syntax: loadavg.py install|remove|start|stop
+  --load|--disk|--proc|--service|--end|--run|--status|--history
+  (frequency) --count=(count) --old=(count)
+  --port=(port) --browse -q -v
 
 If one of install, remove, start, or stop is used, this program will be treated
   as a Windows service.  The other command line parameters are ignored.
 --browse shows the performance counters browse dialog.
+--count specifies how many records to print when a frequency is specified.
 --disk queries the disk load average.
 --end asks a running service to stop.
+--history prints the internal history state.
 --load queries the combined process and disk load average (this is the default
   action).
+--old specifies the number of older values to print.  Older values require that
+  enough history has been collected, and will be spaced at the requested
+  frequency.  This option is only used for --load, --proc, or --disk when a
+  frequency or --count is specified.
 --port specifies the port the service and queries should run on.  Default is
  24047.
---proc queries the process load average
+--proc queries the process load average.
+-q or --quiet decreases the verbosity.
 --run monitors the load and prints it to stdout without providing a service.
 --service starts the service running.  This will continue to run until asked to
   end.
 --status prints the internal state dictionary.
--v increases the verbosity.
+-v or --verbose increases the verbosity.
 
 If a frequency is specified and one of --load, --proc, --disk, or --run is
 specified, the appropriate load average is printed at that frequency.
@@ -387,18 +465,20 @@ running the following:
         win32serviceutil.HandleCommandLine(
             LoadAvgSvc, argv=sys.argv, serviceClassString=LoadAvgSvc.svcPath)
         sys.exit(0)
-    if mode == "run" and Verbose < 2 and not frequency:
-        Verbose = 2
+    if ("count" in opts or "old" in opts) and not frequency:
+        frequency = 5
+    if mode == "run" and verbose < 2 and not frequency:
+        verbose = 2
     if mode == "service" or mode == "run":
-        collector = LoadAvgCollect(verbose=Verbose)
+        collector = LoadAvgCollect(verbose=verbose)
         collector.start()
     if mode == "service":
-        service = LoadAvgService(Verbose, port, collector)
+        service = LoadAvgService(verbose, opts["port"], collector)
         service.start()
     if mode == "service" or (mode == "run" and not frequency):
         while not collector.halt:
             time.sleep(5)
     elif frequency:
-        query_service_loop(mode, port, frequency, collector)
+        query_service_loop(mode, opts, frequency, collector, verbose)
     else:
-        print query_service(mode, port)
+        print query_service(mode, opts["port"])
